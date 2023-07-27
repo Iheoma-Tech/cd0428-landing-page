5dfeb187c5563afdef0480d175d1dce40f9925ca
unknown <obikaonuiheoma@gmail.com>
2023-07-27 16:43:06 -0500

    Add button and section in the viewport logic to the landing page

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Landing Page Project
+This project is a landing page project from the Udacity Frontend Webdevelopment nanodegree.
+It ivolves making the web page responsive by using javaScript.
 
 ## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+The list items were added programatically.
+- Section1
+-Section2
+-Section3
+-Section4
--- js/app.js
+++ js/app.js
@@ -60,3 +60,102 @@
 // Set sections as active
 
 
+// Creating the button Element and storing the array of button in buttons element.
+let buttons =[];
+for(let i = 0; i < 4; i++){
+  let button= document.createElement('button') ;
+  button.textContent = "section" + (i + 1);
+  buttons.push(button);
+}
+console.log('buttons' , buttons)
+
+let ulContainer = document.querySelector('#navbar__list');
+
+// Getting and storing my section tags
+
+const mySection = document.querySelectorAll("section");
+console.log('mySection', mySection);
+
+// Add each of the buttons to the ulContainer as a list
+
+buttons.forEach(function (button, index){
+  button.addEventListener('click', (event)=>{
+    event.preventDefault();
+    console.log('mySection[index]', mySection[index]);
+    mySection[index].scrollIntoView({
+      behavior : "smooth",
+    });
+  });
+    let listItem =document.createElement('li');
+    listItem.appendChild(button);
+    ulContainer.appendChild(listItem);
+    
+});
+
+// Adding eventlistener to the window for scroll event
+
+ document.addEventListener('scroll', () => {
+  console.log ('mySection inviewport', mySection);
+   mySection.forEach(function (section, index) {
+       const sectionPosition = section.getBoundingClientRect();
+      const isInViewport = sectionPosition.top >= 0 && sectionPosition.left > 0
+        && sectionPosition.right <= window.innerWidth && sectionPosition.bottom <= window.innerHeight;
+        
+      if (isInViewport) {
+        section.classList.add('your-active-class');
+        buttons[index].classList.add('your-active-class'); // Add the 'active-button' class to the corresponding button
+      console.log ('mySection inviewport', mySection);
+      console.log('buttons invieport', buttons)
+      } else {
+         section.classList.remove('your-active-class');
+         buttons[index].classList.remove('your-active-class'); // Add the 'active-button' class to the corresponding button
+      }
+    });
+  })
+
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
+
+
+
